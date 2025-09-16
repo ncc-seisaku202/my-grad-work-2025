1a196261c3fb794de8a9bd778c7e06bebd18679f
NCC_KANSAI_SATO <kss-23180026@nsgcl.jp>
2025-09-16 15:57:52 +0900

    feat: UIライブラリをChakra UIからMUIに移行 #7

--- baseball-sns/src/main.jsx
+++ baseball-sns/src/main.jsx
@@ -1,10 +1,23 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.jsx'
 
+const darkTheme = createTheme({
+  palette: { mode: 'dark' },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
